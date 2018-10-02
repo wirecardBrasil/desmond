34133a9a0fe2137252c4f813d355b922bace9de1
Lucas Queiroz <lucas.queiroz@moip.com.br>
2018-10-02 11:37:51 -0300

    Rename test class

--- bank/src/test/kotlin/br/com/wirecard/desmond/helper/BankGeneratorHelperTest.kt
+++ bank/src/test/kotlin/br/com/wirecard/desmond/helper/BankGeneratorHelperTest.kt
@@ -2,15 +2,15 @@ package br.com.wirecard.desmond.helper
 
 import com.winterbe.expekt.should
 import org.junit.Test
-import br.com.wirecard.desmond.helper.BankGeneratorHelper as bankValidatorHelper
+import br.com.wirecard.desmond.helper.BankGeneratorHelper as bankGeneratorHelper
 import br.com.wirecard.desmond.helper.HelperObjectFactory as objectFactory
 
-class BankValidatorHelperTest {
+class BankGeneratorHelperTest {
     @Test
     fun shouldReturnCorrectResultWhenMultiplyingByWeight() {
         val fakeNumber = objectFactory.FAKE_NUMBER
         val fakeWeight = objectFactory.FAKE_WEIGHT
         val result = objectFactory.EXPECTED_OUTPUT_MULTIPLY_WEIGHT
-        bankValidatorHelper.multiplyByWeight(fakeNumber, fakeWeight).should.equal(result)
+        bankGeneratorHelper.multiplyByWeight(fakeNumber, fakeWeight).should.equal(result)
     }
 }
