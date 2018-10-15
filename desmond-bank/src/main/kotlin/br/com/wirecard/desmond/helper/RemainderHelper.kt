d35f2f7a31485c6a4179f8a0897c737d132d98f5
Juliana Myaki <juliana.silva@moip.com.br>
2018-10-15 15:59:31 -0300

    Passing multiplier parameter

--- desmond-bank/src/main/kotlin/br/com/wirecard/desmond/helper/RemainderHelper.kt
+++ desmond-bank/src/main/kotlin/br/com/wirecard/desmond/helper/RemainderHelper.kt
@@ -8,9 +8,9 @@ object RemainderHelper {
         calculator: RemainderCalculator = RemainderCalculator.Default,
         multiplier: Int = 1
     ) = when (calculator) {
-        RemainderCalculator.Default -> calculateRemainder(number, weight, mod) { value -> value }
-        RemainderCalculator.LastDigitOnly -> calculateRemainder(number, weight, mod) { value -> value % 10 }
-        RemainderCalculator.SumDigits -> calculateRemainder(number, weight, mod) { value -> (value / 10) + (value % 10) }
+        RemainderCalculator.Default -> calculateRemainder(number, weight, mod, multiplier) { value -> value }
+        RemainderCalculator.LastDigitOnly -> calculateRemainder(number, weight, mod, multiplier) { value -> value % 10 }
+        RemainderCalculator.SumDigits -> calculateRemainder(number, weight, mod, multiplier) { value -> (value / 10) + (value % 10) }
     }
 
     private fun calculateRemainder(number: String, weight: Array<Int>, mod: Int, multiplier: Int = 1, operator: (value: Int) -> Int): Int {
